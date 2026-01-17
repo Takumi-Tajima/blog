22c2b68e7d0380e5a2d2faa7e5a89091875c304c
taji <taji@sonicgarden.jp>
2026-01-17 18:04:59 +0900

    管理者 articles

---
+++ app/controllers/admins/articles_controller.rb
@@ -0,0 +1,45 @@
+class Admins::ArticlesController < Admins::ApplicationController
+  before_action :set_article, only: %i[show edit update]
+
+  def index
+    @articles = Article.default_order
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to admins_article_path(@article), notice: '記事を作成しました。'
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to admins_article_path(@article), notice: '記事を更新しました。'
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params.expect(:id))
+  end
+
+  def article_params
+    params.expect(article: %i[title body published_at])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'articles#index'
+
+  namespace :admins do
+    root 'articles#index'
+    resources :articles, only: %i[index show new edit create update]
+  end
+
   resources :articles, only: %i[index show]
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
